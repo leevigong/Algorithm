606c753cafac8f5fe643172c1db5af12ea1bad73
leevigong <107829121+leevigong@users.noreply.github.com>
2024-05-25 20:56:37 +0900

    [Bronze V] Title: 홍익대학교, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int var = n-1946;
+        System.out.println(var);
+    }
+}
