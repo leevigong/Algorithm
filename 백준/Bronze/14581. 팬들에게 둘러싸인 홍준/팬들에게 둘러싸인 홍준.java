c3db4709ef24849c6b7cc08a911617a47a622eee
leevigong <107829121+leevigong@users.noreply.github.com>
2024-05-26 17:16:59 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 200 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.printf(":fan::%s::fan:\n",id);
+        System.out.println(":fan::fan::fan:");
+    }
+}
