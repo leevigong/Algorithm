58ea68c4ba09d11c33265818d12efb46c10fe2aa
leevigong <107829121+leevigong@users.noreply.github.com>
2024-05-28 01:51:44 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 204 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        if (name.equals("NLCS")){
+            System.out.println("North London Collegiate School");
+        }
+        else if (name.equals("BHA")){
+            System.out.println("Branksome Hall Asia");
+        }
+        else if (name.equals("KIS")){
+            System.out.println("Korea International School");
+        }
+        else if (name.equals("SJA")){
+            System.out.println("St. Johnsbury Academy");
+        }
+    }
+}
