55d469450da1ce63348cf48a6ee67cbb24120acb
Lee Daeun <107829121+leevigong@users.noreply.github.com>
2024-10-10 02:54:06 +0900

    [Silver III] Title: 이친수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,7 +1,16 @@
 N = int(input())
-fi = [1] * N
 
-for i in range(2, N):
-    fi[i] = fi[i-1] + fi[i-2]
+a = [0] * (N + 1) # 끝자리가 0
+b = [0] * (N + 1) # 끝자리가 1
 
-print(fi[N-1])
+if N >= 1:
+    b[1] = 1  # n=0일떄 '1'
+if N >= 2:
+    a[2] = 1  # n=1일때 '10'
+
+for i in range(2, N + 1):
+    a[i] = a[i - 1] + b[i - 1]  # 끝자리가 0인 경우
+    b[i] = a[i - 1]             # 끝자리가 1인 경우
+
+
+print(a[N]+b[N])
